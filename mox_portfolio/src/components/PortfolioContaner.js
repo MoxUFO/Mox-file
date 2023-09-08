623fd3de54c3360c55011bb3afb21bedccee6dd7
MOX UFO <mox.ufo@gmail.com>
2023-09-07 21:12:53 -0400

    app backround extended and project container now display 100%

--- mox_portfolio/src/components/PortfolioContaner.js
+++ mox_portfolio/src/components/PortfolioContaner.js
@@ -3,6 +3,7 @@ import TopLabel from './TopLabel'
 import NavBar from './NavBar'
 import Intro from './Intro'
 import Projects from './Projects'
+import '../assests/css/PortfolioContainer.css'
 
 export default function PortfolioContaner() {
   return (
