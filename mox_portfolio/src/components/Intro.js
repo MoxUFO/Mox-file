ebb8c0b8a3443919d04838aaefb30a9090b1bccd
MOX UFO <mox.ufo@gmail.com>
2023-09-07 20:08:57 -0400

    intro displays in generic design

--- mox_portfolio/src/components/Intro.js
+++ mox_portfolio/src/components/Intro.js
@@ -6,10 +6,13 @@ export default function Intro() {
   return (
     <div className="intro">
       <Card  className="intro-card">
+        <div className="card-content">
         <h1>Hello, I'm Nelson Foster Jr</h1>
         <h1>Full Stack Web Developer </h1>
         <h1>Welocome to my Portfolio</h1>
+        </div>
       </Card>
+
     </div>
   );
 }
